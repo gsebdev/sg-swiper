a98efe0e6250390fb64533bfb9b451c68be97f01
Sebastien Gault <sgault.webdev@gmail.com>
2024-02-10 17:06:48 +0100

    Resolved Issue #2 : improve slide drag stick to closest slide from middle ...

--- index.ts
+++ index.ts
@@ -1,10 +1,16 @@
 import Swiper from "./src/sg-swiper";
 
 document.addEventListener("DOMContentLoaded", () => {
-    if(document.querySelector(".swiper-demo")) {
-        new Swiper(document.querySelector(".swiper-demo") as HTMLElement, {
-            slideClassName: "swiper-slide",
-            draggable: true
-        } );
-    }
+  if (document.querySelector(".swiper-demo")) {
+    const swiper = new Swiper(
+      document.querySelector(".swiper-demo") as HTMLElement,
+      {
+        slideClassName: "swiper-slide",
+        draggable: true,
+        onSlideClick: (index) => {
+          swiper.index = index;
+        },
+      }
+    );
+  }
 });
